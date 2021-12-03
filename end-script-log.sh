8008c496a4c7b905cfd8d989c3c3ca102c17e3bb
edneyRoldao <edneyroldao@gmail.com>
2021-12-03 08:09:28 -0300

    ending script has been updated

--- end-script-log.sh
+++ end-script-log.sh
@@ -6,5 +6,5 @@ echo "Creating ending file time log"
 touch end-time-process-log.txt
 
 echo "Just kidding"
-google-chrome https://www.youtube.com/watch?v=o6DLmuibSVE&ab_channel=JasonVoorhees
+xdg-open https://www.youtube.com/watch?v=o6DLmuibSVE&ab_channel=JasonVoorhees
 
