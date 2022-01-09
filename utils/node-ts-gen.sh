a1057277d1e56b97ec140f43a7740939fd0d327e
edneyRoldao <edneyroldao@gmail.com>
2022-01-08 21:46:45 -0300

    node project generator finished

--- utils/node-ts-gen.sh
+++ utils/node-ts-gen.sh
@@ -1,146 +1,232 @@
 #!/bin/bash
-echo "###############################################"
-echo "##### NODE PROJECT GENERATOR - TYPESCRIPT #####"
-echo "###############################################"
-echo " The first parameter should be the name of project"
+echo "#########################################################"
+echo "################ NODE PROJECT GENERATOR #################"
+echo "########### typescript, jest, estlint, babel ############"
+echo "#########################################################"
+echo "***** LOG node and yarn should be installed"
+echo "***** LOG The first parameter should be the name of project"
 
-echo "-----------------------------------------------------"
-echo "######## LOG - creating project folder from current folder"
+if [ -z "$1" ]
+then
+echo "The project name was not provided as parameter"
+echo "exec example: node-ts-gen.sh project-name"
+echo "process terminated. exit from bash"
+
+else
+echo "-------------------------------------"
+echo "***** LOG - creating folder structure"
+
+if [ -d "$1" ]
+then
+    rm -r $1
+fi
+
 mkdir $1
 cd $1
-
-echo "-----------------------------------------------------"
-echo "######## LOG - creating project"
-yarn init -y
-
-echo "-----------------------------------------------------"
-echo "######## LOG - installing typescript as dev dependency"
-yarn add typescript -D
-
-echo "-----------------------------------------------------"
-echo "######## LOG - creating tsconfig file"
-echo "{" >> tsconfig.json
-echo "  \"compilerOptions\": {" >> tsconfig.json
-echo "    \"rootDir\": \"./src\"," >> tsconfig.json
-echo "    \"outDir\": \"./dist\"," >> tsconfig.json
-echo "    \"target\": \"es2019\"," >> tsconfig.json
-echo "    \"module\": \"commonjs\"," >> tsconfig.json
-echo "    \"lib\": [\"es6\"]," >> tsconfig.json
-echo "    \"typeRoots\": [" >> tsconfig.json
-echo "      \"./node_modules/@types\"," >> tsconfig.json
-echo "      \"./src/@types\"" >> tsconfig.json
-echo "    ]," >> tsconfig.json
-echo "    \"strict\": true," >> tsconfig.json
-echo "    \"allowJs\": true," >> tsconfig.json
-echo "    \"skipLibCheck\": true," >> tsconfig.json
-echo "    \"noImplicitAny\": true," >> tsconfig.json
-echo "    \"removeComments\": true," >> tsconfig.json
-echo "    \"esModuleInterop\": true," >> tsconfig.json
-echo "    \"resolveJsonModule\": true," >> tsconfig.json
-echo "    \"emitDecoratorMetadata\": true," >> tsconfig.json
-echo "    \"experimentalDecorators\": true," >> tsconfig.json
-echo "    \"forceConsistentCasingInFileNames\": true," >> tsconfig.json
-echo "    \"baseUrl\": \".\"," >> tsconfig.json
-echo "    \"paths\": {" >> tsconfig.json
-echo "      \"@controllers/*\": [\"./src/controllers/*\"]," >> tsconfig.json
-echo "      \"@models/*\": [\"./src/models/*\"]," >> tsconfig.json
-echo "      \"@config/*\": [\"./src/config/*\"]," >> tsconfig.json
-echo "      \"@views/*\": [\"./src/views/*\"]" >> tsconfig.json
-echo "    }" >> tsconfig.json
-echo "  }," >> tsconfig.json
-echo "  \"include\": [" >> tsconfig.json
-echo "    \"src/**/*\"" >> tsconfig.json
-echo "  ]" >> tsconfig.json
-echo "}" >> tsconfig.json
-
-echo "-----------------------------------------------------"
-echo "######## LOG - installing typescript as dev dependency"
-yarn add ts-node-dev -D
-yarn add tsconfig-paths -D
-yarn add @types/node -D
-
-echo "-----------------------------------------------------"
-echo "######## LOG - installing express"
-yarn add express
-yarn add @types/express -D
-
-echo "-----------------------------------------------------"
-echo "######## LOG - creating source folders"
 mkdir src
-mkdir src/tests
+mkdir test
+mkdir test/functional
+mkdir src/util
+mkdir src/test
 mkdir src/views
 mkdir src/config
 mkdir src/models
 mkdir src/controllers
 
-echo "-----------------------------------------------------"
-echo "######## LOG - creating server.ts"
-echo "import express from 'express';" >> src/server.ts
-echo "" >> src/server.ts
-echo "const app = express();" >> src/server.ts
-echo "" >> src/server.ts
-echo "app.get('/', (req, res) => {" >> src/server.ts
-echo "   return res.json({message: 'Hello World'});" >> src/server.ts
-echo "})" >> src/server.ts
-echo "" >> src/server.ts
-echo "app.listen(3000);" >> src/server.ts
-echo "" >> src/server.ts
+
+echo "-------------------------------------"
+echo "***** LOG - Creating node project (package.json)"
+echo "{" >> package.json
+echo "  \"name\": \"$1\"," >> package.json
+echo "  \"version\": \"1.0.0\"," >> package.json
+echo "  \"description\": \"Node Project base with typescript, eslint and jest.\"," >> package.json
+echo "  \"main\": \"src/index.js\"," >> package.json
+echo "  \"author\": \"$USER\"," >> package.json
+echo "  \"license\": \"ISC\"," >> package.json
+echo "  \"keywords\": []," >> package.json
+echo "  \"scripts\": {" >> package.json
+echo "    \"build\": \"tsc\"," >> package.json
+echo "    \"start\": \"yarn build && node dist/src/index.js\"," >> package.json
+echo "    \"start:dev\": \"ts-node-dev --respawn --transpile-only --ignore-watch node_modules src/index.ts\"," >> package.json
+echo "    \"test\": \"jest\"," >> package.json
+echo "    \"test:functional\": \"jest --projects ./test --runInBand\"," >> package.json
+echo "    \"lint\": \"eslint ./src ./test --ext .ts\"," >> package.json
+echo "    \"lint:fix\": \"eslint ./src ./test --ext .ts --fix\"" >> package.json
+echo "  }" >> package.json
+echo "}" >> package.json
+
+
+echo "-------------------------------------"
+echo "***** LOG - Installing dev dependencies"
+yarn add -D jest
+yarn add -D eslint
+yarn add -D ts-jest
+yarn add -D supertest
+yarn add -D typescript
+yarn add -D ts-node-dev
+yarn add -D @types/node
+yarn add -D @types/jest
+yarn add -D @types/express
+yarn add -D @types/supertest
+yarn add -D @types/module-alias
+yarn add -D @typescript-eslint/parser
+yarn add -D @typescript-eslint/eslint-plugin
+
+
+echo "-------------------------------------"
+echo "***** LOG - Installing dependencies"
+yarn add express
+yarn add module-alias
+
+
+echo "-------------------------------------"
+echo "***** LOG - Creating tsconfig.json file"
+echo "{" >> tsconfig.json
+echo "  \"compilerOptions\": {" >> tsconfig.json
+echo "    \"target\": \"es2019\"," >> tsconfig.json
+echo "    \"moduleResolution\": \"node\"," >> tsconfig.json
+echo "    \"module\": \"commonjs\"," >> tsconfig.json
+echo "    \"lib\": [\"es2019\"]," >> tsconfig.json
+echo "    \"sourceMap\": true," >> tsconfig.json
+echo "    \"outDir\": \"dist\"," >> tsconfig.json
+echo "    \"strict\": true," >> tsconfig.json
+echo "    \"alwaysStrict\": true," >> tsconfig.json
+echo "    \"strictNullChecks\": true," >> tsconfig.json
+echo "    \"strictFunctionTypes\": true," >> tsconfig.json
+echo "    \"noImplicitAny\": true," >> tsconfig.json
+echo "    \"noImplicitThis\": true," >> tsconfig.json
+echo "    \"noImplicitReturns\": true," >> tsconfig.json
+echo "    \"resolveJsonModule\": true," >> tsconfig.json
+echo "    \"removeComments\": true," >> tsconfig.json
+echo "    \"esModuleInterop\": true," >> tsconfig.json
+echo "    \"allowSyntheticDefaultImports\": true," >> tsconfig.json
+echo "    \"baseUrl\": \".\"," >> tsconfig.json
+echo "    \"paths\": {" >> tsconfig.json
+echo "      \"@src/*\": [\"./src/*\"]," >> tsconfig.json
+echo "      \"@test/*\": [\"./test/*\"]" >> tsconfig.json
+echo "    }," >> tsconfig.json
+echo "    \"rootDirs\": [" >> tsconfig.json
+echo "      \"./src\"," >> tsconfig.json
+echo "      \"./test\"" >> tsconfig.json
+echo "    ]," >> tsconfig.json
+echo "    \"emitDecoratorMetadata\": true," >> tsconfig.json
+echo "    \"experimentalDecorators\": true" >> tsconfig.json
+echo "  }," >> tsconfig.json
+echo "  \"include\": [" >> tsconfig.json
+echo "    \"./src/**/*.ts\"," >> tsconfig.json
+echo "    \"./test/**/*.ts\"" >> tsconfig.json
+echo "  ]," >> tsconfig.json
+echo "  \"exclude\": [" >> tsconfig.json
+echo "    \"./node_modules/*\"," >> tsconfig.json
+echo "    \"dist\"" >> tsconfig.json
+echo "  ]" >> tsconfig.json
+echo "}" >> tsconfig.json
+
+echo "-------------------------------------"
+echo "***** LOG - Creating .eslintrc file config"
+echo "{" >> .eslintrc
+echo "  \"root\": true," >> .eslintrc
+echo "  \"parser\": \"@typescript-eslint/parser\"," >> .eslintrc
+echo "  \"plugins\": [" >> .eslintrc
+echo "    \"@typescript-eslint\"" >> .eslintrc
+echo "  ]," >> .eslintrc
+echo "  \"extends\": [" >> .eslintrc
+echo "    \"eslint:recommended\"," >> .eslintrc
+echo "    \"plugin:@typescript-eslint/eslint-recommended\"," >> .eslintrc
+echo "    \"plugin:@typescript-eslint/recommended\"" >> .eslintrc
+echo "  ]," >> .eslintrc
+echo "  \"rules\": {" >> .eslintrc
+echo "  }" >> .eslintrc
+echo "}" >> .eslintrc
+
+echo "-------------------------------------"
+echo "***** LOG - Creating global jest.config.js file"
+echo "const { resolve } = require('path');" >> jest.config.js
+echo "const root = resolve(__dirname);" >> jest.config.js
+echo "" >> jest.config.js
+echo "module.exports = {" >> jest.config.js
+echo "  rootDir: root," >> jest.config.js
+echo "  displayName: 'root-tests'," >> jest.config.js
+echo "  testMatch: ['<rootDir>/src/**/*.test.ts']," >> jest.config.js
+echo "  testEnvironment: 'node'," >> jest.config.js
+echo "  preset: 'ts-jest'," >> jest.config.js
+echo "  clearMocks: true," >> jest.config.js
+echo "  moduleNameMapper: {" >> jest.config.js
+echo "    '@src/(.*)': '<rootDir>/src/\$1'," >> jest.config.js
+echo "    '@test/(.*)': '<rootDir>/test/\$1'" >> jest.config.js
+echo "  }" >> jest.config.js
+echo "}" >> jest.config.js
+echo "" >> jest.config.js
+
+
+echo "-------------------------------------"
+echo "***** LOG - Creating jest.config.js file"
+echo "const { resolve } = require('path');" >> test/jest.config.js
+echo "const root = resolve(__dirname, '..');" >> test/jest.config.js
+echo "const rootConfig = require(\`\${root}/jest.config.js\`);" >> test/jest.config.js
+echo "" >> test/jest.config.js
+echo "module.exports = {" >> test/jest.config.js
+echo "  ...rootConfig," >> test/jest.config.js
+echo "  ...{" >> test/jest.config.js
+echo "    rootDir: root," >> test/jest.config.js
+echo "    displayName: 'e2e-tests'," >> test/jest.config.js
+echo "    testMatch: ['<rootDir>/test/**/*.test.ts']," >> test/jest.config.js
+echo "    setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts']" >> test/jest.config.js
+echo "  }" >> test/jest.config.js
+echo "}" >> test/jest.config.js
+echo "" >> test/jest.config.js
+
+
+echo "-------------------------------------"
+echo "***** LOG - Creating jest-setup.ts file"
+echo "" >> test/jest-setup.ts
+
+
+echo "-------------------------------------"
+echo "***** LOG - Adding Module aliais config"
+echo "import * as path from 'path';" >> src/config/module-alias.ts
+echo "import moduleAlias from 'module-alias';" >> src/config/module-alias.ts
+echo "" >> src/config/module-alias.ts
+echo "const files = path.resolve(__dirname, '../..');" >> src/config/module-alias.ts
+echo "" >> src/config/module-alias.ts
+echo "moduleAlias.addAliases({" >> src/config/module-alias.ts
+echo "  '@src': path.join(files, 'src')," >> src/config/module-alias.ts
+echo "  '@test': path.join(files, 'test')" >> src/config/module-alias.ts
+echo "});" >> src/config/module-alias.ts
+echo "" >> src/config/module-alias.ts
 
 echo "-----------------------------------------------------"
-echo "######## LOG - Config Jest"
-yarn add jest -D
-yarn add ts-jest -D
-yarn add @types/jest -D
-
-echo "import { compilerOptions } from './tsconfig.json';" >> jest.config.ts
-echo "import { pathsToModuleNameMapper } from 'ts-jest/utils';" >> jest.config.ts
-echo "" >> jest.config.ts
-echo "export default {" >> jest.config.ts
-echo "  clearMocks: true," >> jest.config.ts
-echo "  resetMocks: true," >> jest.config.ts
-echo "  preset: 'ts-jest'," >> jest.config.ts
-echo "  coverageDirectory: './coverage'," >> jest.config.ts
-echo "  testMatch: [" >> jest.config.ts
-echo "    '**/__tests__/**/*.[jt]s?(x)'," >> jest.config.ts
-echo "    '**/?(*.)+(spec|test).[tj]s?(x)'" >> jest.config.ts
-echo "  ]," >> jest.config.ts
-echo "  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' })" >> jest.config.ts
-echo "}" >> jest.config.ts
+echo "***** LOG - creating main file index.ts"
+echo "import express from 'express';" >> src/index.ts
+echo "const port = process.env.port || 3000;" >> src/index.ts
+echo "" >> src/index.ts
+echo "const app = express();" >> src/index.ts
+echo "" >> src/index.ts
+echo "app.get('/sample', (req, res) => {" >> src/index.ts
+echo "   return res.json({message: 'Hello World'});" >> src/index.ts
+echo "})" >> src/index.ts
+echo "" >> src/index.ts
+echo "app.listen(port, () => {" >> src/index.ts
+echo "  console.log('the $1 app, is working properly on port: ', port)" >> src/index.ts
+echo "});" >> src/index.ts
 
 echo "-----------------------------------------------------"
-echo "######## LOG - Config eslint"
-yarn add eslint -D
-yarn add @typescript-eslint/parser -D
-yarn add @typescript-eslint/eslint-plugin -D
+echo "***** LOG - adding a test sample"
+echo "" >> test/functional/sample.test.ts
+echo "describe('This is a functional sample test', () => {" >> test/functional/sample.test.ts
+echo "  it('This is the first test', async() => {" >> test/functional/sample.test.ts
+echo "    const status = 200;" >> test/functional/sample.test.ts
+echo "    const body = {message: 'Hello World'};" >> test/functional/sample.test.ts
+echo "    expect(status).toBe(200);" >> test/functional/sample.test.ts
+echo "    expect(body).toStrictEqual({message: 'Hello World'});" >> test/functional/sample.test.ts
+echo "  });" >> test/functional/sample.test.ts
+echo "});" >> test/functional/sample.test.ts
 
-echo "{" >> .eslintrc.json
-echo "  \"env\": {" >> .eslintrc.json
-echo "     \"es2021\": true," >> .eslintrc.json
-echo "     \"node\": true," >> .eslintrc.json
-echo "     \"jest\": true" >> .eslintrc.json
-echo "   }," >> .eslintrc.json
-echo "  \"parser\": \"@typescript-eslint/parser\"," >> .eslintrc.json
-echo "  \"parserOptions\": {" >> .eslintrc.json
-echo "     \"ecmaVersion\": 13," >> .eslintrc.json
-echo "     \"sourceType\": \"module\"" >> .eslintrc.json
-echo "   }," >> .eslintrc.json
-echo "  \"plugins\": [" >> .eslintrc.json
-echo "    \"@typescript-eslint\"" >> .eslintrc.json
-echo "   ]," >> .eslintrc.json
-echo "  \"extends\": [" >> .eslintrc.json
-echo "    \"eslint:recommended\"," >> .eslintrc.json
-echo "    \"plugin:@typescript-eslint/eslint-recommended\"," >> .eslintrc.json
-echo "    \"plugin:@typescript-eslint/recommended\"" >> .eslintrc.json
-echo "   ]," >> .eslintrc.json
-echo "  \"rules\": {" >> .eslintrc.json
-echo "   }" >> .eslintrc.json
-echo "}" >> .eslintrc.json
+echo "test('this is a sample test', () => {" >> src/test/sample.test.ts
+echo "  const test = 'sample';" >> src/test/sample.test.ts
+echo "" >> src/test/sample.test.ts
+echo "  expect(test).toEqual('sample');" >> src/test/sample.test.ts
+echo "});" >> src/test/sample.test.ts
+echo "" >> src/test/sample.test.ts
 
-echo "-----------------------------------------------------"
-echo "######## LOG - adding run script"
-sed -i '6i\  "scripts": {' package.json
-sed -i '7i\     "dev": "ts-node-dev -r tsconfig-paths/register --respawn --ignore-watch node_modules --no-notify src/server.ts",' package.json
-sed -i '8i\     "test": "test",' package.json
-sed -i '9i\     "lint": "eslint ./src ./test --ext .ts",' package.json
-sed -i '10i\     "lint:fix": "eslint ./src ./test --ext .ts --fix"' package.json
-sed -i '11i\  },' package.json
+fi
