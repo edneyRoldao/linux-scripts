0f807154db3dd454d38e750d093e02949f3ee5aa
Edy <edneyroldao@gmail.com>
2022-07-30 19:09:42 -0300

    Update install-maven.sh
    changing maven 3.8 to 3.6.3

--- dev-tools-install/binary-install/install-maven.sh
+++ dev-tools-install/binary-install/install-maven.sh
@@ -2,21 +2,21 @@ echo "#######################################################"
 echo "### APACHE MAVEN - install process has been started ###"
 echo "#######################################################"
 
-echo "LOG - Downloading Apache Maven 3.8.4 ..."
-wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
-tar -xzf apache-maven-3.8.4-bin.tar.gz
-rm apache-maven-3.8.4-bin.tar.gz
+echo "LOG - Downloading Apache Maven 3.6.3 ..."
+wget https://dlcdn.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xzf apache-maven-3.6.3-bin.tar.gz
+rm apache-maven-3.6.3-bin.tar.gz
 
-echo "installing Apache Maven into /opt/apache-maven-3.8.4 ..."
-if [ -d "/opt/apache-maven-3.8.4" ];then
-	sudo rm -rf /opt/apache-maven-3.8.4
+echo "installing Apache Maven into /opt/apache-maven-3.6.3 ..."
+if [ -d "/opt/apache-maven-3.6.3" ];then
+	sudo rm -rf /opt/apache-maven-3.6.3
 fi
-sudo mv apache-maven-3.8.4 /opt/apache-maven-3.8.4
+sudo mv apache-maven-3.8.4 /opt/apache-maven-3.6.3
 
 echo "LOG - Setting up MAVEN_HOME, M2_HOME and M2 environments for: Maven ..."
 echo " " >> ~/.bashrc
 echo "# MAVEN environment config" >> ~/.bashrc
-echo "M2_HOME=/opt/apache-maven-3.8.4" >> ~/.bashrc
+echo "M2_HOME=/opt/apache-maven-3.6.3" >> ~/.bashrc
 echo "export M2_HOME" >> ~/.bashrc
 echo "M2=\$M2_HOME/bin" >> ~/.bashrc
 echo "export M2" >> ~/.bashrc
